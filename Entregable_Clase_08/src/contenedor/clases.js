b89febed03a5ed1ab68b738d88a576282b1e36b9
Julieta Silva <juuli.siilva@gmail.com>
2023-06-13 01:07:07 -0300

    carrito arreglado

--- Entregable_Clase_08/src/contenedor/clases.js
+++ Entregable_Clase_08/src/contenedor/clases.js
@@ -100,3 +100,78 @@ export class ProductManager {
     }
 }
   
+export class CartManager {
+    constructor(path) {
+      this.path = path;
+      this.carts = [];
+      this.lastCartId = 0;
+      this.loadCarts();
+    }
+  
+    async loadCarts() {
+      try {
+        const data = await fs.readFileSync(this.path);
+        this.carts = JSON.parse(data);
+        this.lastCartId = this.carts.reduce((maxId, cart) => Math.max(maxId, cart.id), 0);
+      } catch (error) {
+        this.carts = [];
+        this.lastCartId = 0;
+      }
+    }
+  
+    generateCartId() {
+      this.lastCartId++;
+      return this.lastCartId.toString();
+    }
+  
+    async saveCarts() {
+      try {
+        await fs.writeFileSync(this.path, JSON.stringify(this.carts, null, 2));
+      } catch (error) {
+        console.log('Error al guardar los carritos', error);
+      }
+    }
+  
+    async getCartById(cartId) {
+      try {
+        const cart = this.carts.find(c => c.id === cartId);
+        return cart;
+      } catch (error) {
+        console.log('Error al obtener el carrito', error);
+      }
+    }
+  
+    async createCart() {
+      try {
+        const cart = {
+          id: parseInt(this.generateCartId()),
+          products: []
+        };
+        this.carts.push(cart);
+        await this.saveCarts();
+        return cart;
+      } catch (error) {
+        console.log('Error al crear el carrito', error);
+      }
+    }
+  
+    async addProductToCart(cartId, productId) {
+      try {
+        const cart = await this.getCartById(cartId);
+        if (cart) {
+          const existingProduct = cart.products.find(product => product.product === productId);
+          if (existingProduct) {
+            existingProduct.quantity += 1;
+          } else {
+            cart.products.push({ product: productId, quantity: 1 });
+          }
+          await this.saveCarts();
+          return cart;
+        } else {
+          console.log('Carrito no encontrado');
+        }
+      } catch (error) {
+        console.log('Error al agregar el producto al carrito', error);
+      }
+    }
+  }
--- Entregable_Clase_08/src/rutas/carrito.js
+++ Entregable_Clase_08/src/rutas/carrito.js
@@ -1,15 +1,16 @@
 import express from "express";
 import { ProductManager } from "../contenedor/clases.js";
+import { CartManager } from "../contenedor/clases.js";
 const rutaCarrito = express.Router();
 
-const carritos = new ProductManager('src/db/carritos.json')
+const carritos = new CartManager('src/db/carritos.json')
 const productos = new ProductManager('src/db/productos.json');
 
 //ENDPOINTS ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 rutaCarrito.get('/:cid', async(req, res)=>{
     const idCarrito = parseInt(req.params.cid);
-    const carrito = await carritos.getProductById(idCarrito)
+    const carrito = await carritos.getCartById(idCarrito)
     if(carrito){
         res.json(carrito.products)
     } else {
@@ -19,18 +20,24 @@ rutaCarrito.get('/:cid', async(req, res)=>{
 });
 
 rutaCarrito.post('/', async(req,res)=>{
-    const carrito = {
-        id: parseInt(carritos.generateCode()),
-        date: Date.now(),
-        productos: []
-      };
+    const carrito = await carritos.createCart();
     res.json(carrito);
 })
 
-//rutaCarrito.post('/:cid/product/:pid', async(req,res)=>{
-//    const idCarrito = parseInt(req.params.cid)
-//    const idProducto = parseInt(req.params.pid)
-//})
+rutaCarrito.post('/:cid/product/:pid', async(req,res)=>{
+    const idCarrito = parseInt(req.params.cid);
+    const idProducto = parseInt(req.params.pid);
+    const carrito = await carritos.getCartById(idCarrito)
+
+    if(!carrito){
+        await carritos.createCart();
+        await carritos.addProductToCart(idCarrito, idProducto);
+        res.json(`Carrito con id ${idCarrito} creado y producto con id ${idProducto} agregado`)
+    } else {
+        await carritos.addProductToCart(idCarrito,idProducto);
+        res.json(`Producto agregado al carrito ${idCarrito}`)
+    }
+})
 
 
 
